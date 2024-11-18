49e94b9821f7619746d8f286871ed4ca87196fde
luciayn-acc <lucia.yan.wu@accenture.com>
2024-11-18 10:58:21 +0100

    Update app

---
+++ app/app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from ui import display_ui  # Import your UI from ui.py
+
+# Main function to run the Streamlit app
+def main():
+    display_ui()  # Call the function to render the UI from ui.py
+
+if __name__ == "__main__":
+    main()
